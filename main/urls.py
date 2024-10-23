bb348879be1a2bd2eaed1d9257c4254955a69b54
Ergwynt <aday18005@gmail.com>
2024-10-23 20:18:13 +0100

    com

--- main/urls.py
+++ main/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,10 +24,12 @@ from shared import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/'),
+    path('users/', views.users, name='users'),
     path('waves/', include('waves.urls')),
     path('echos/', include('echos.urls')),
     path('shared/', include('shared.urls')),
+    path('@<username>/', views.profile, name='profile'),
     path('login/', views.user_login),
     path('logout/', views.user_logout),
-]
+    path('register/', views.register, name='register'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- shared/views.py
+++ shared/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 
-from shared.forms import LoginForm
+from shared.forms import LoginForm, SignupForm
 
 
 def user_login(request):
     if request.method == 'POST':
-        if (form := LoginForm(request.POST)).is_valid():
+        if (form := LoginForm(request.)).is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             if user := authenticate(request, username=username, password=password):
@@ -20,3 +20,14 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('home')
+
+
+def register(request):
+    if request.method == 'POST':
+        if (form := SignupForm(request.)).is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', dict(form=form))
